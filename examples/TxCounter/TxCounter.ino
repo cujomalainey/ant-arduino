a6438b05edf73206560b4214c23000e1bb224111
Curtis Malainey <curtis@malainey.com>
2016-12-26 23:06:49 -0700

    Completed Tx example and tested Tx message

--- examples/TxCounter/TxCounter.ino
+++ examples/TxCounter/TxCounter.ino
@@ -1,11 +1,9 @@
 /***********************************
- * Ant Open Channel Example
+ * Ant Tx Counter Example
  *
  * Opens a channel with the public
  * network key and transmits data
  * increments in value every second
- *
- * NOTE: THIS EXAMPLE IS INCOMPLETE
  *
  * Author Curtis Malainey
  ************************************/
@@ -13,6 +11,8 @@
 #include "ANT.h"
 #define BAUD_RATE 9600
 Ant ant = Ant();
+
+uint8_t buffer[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 void parseMessage();
 void parseEventMessage(uint8_t code);
@@ -25,6 +25,7 @@ void setup()
 	ChannelId ci;
 	ChannelPeriod cp;
 	ChannelRfFrequency crf;
+	BroadcastMsg bm;
 	OpenChannel oc;
 
 	Serial1.begin(BAUD_RATE);
@@ -43,7 +44,7 @@ void setup()
 
 	ac = AssignChannel();
 	ac.setChannel(0);
-	ac.setChannelType(CHANNEL_TYPE_BIDIRECTIONAL_TRANSMIT);
+	ac.setChannelType(CHANNEL_TYPE_BIDIRECTIONAL_TRANSMIT); //can't wildcard this
 	ac.setChannelNetwork(0);
 	ant.send(ac);
 	parseMessage();
@@ -68,6 +69,12 @@ void setup()
 	ant.send(crf);
 	parseMessage();
 
+	bm = BroadcastMsg();
+	bm.setData(buffer);
+	bm.setChannel(0);
+	ant.send(bm);
+	parseMessage();
+
 	oc = OpenChannel();
 	oc.setChannel(0);
 	ant.send(oc);
@@ -81,67 +88,47 @@ void loop()
 
 void parseMessage() {
 	ant.readPacket();
-	while (ant.getResponse().isAvailable() || ant.getResponse().isError())
+	if(ant.getResponse().isAvailable())
 	{
-		if (ant.getResponse().isAvailable())
-		{
-			uint8_t msgId = ant.getResponse().getMsgId();
-			switch (msgId) {
-				case CHANNEL_EVENT:
-				{
-					ChannelEventResponse cer = ChannelEventResponse();
-					ant.getResponse().getChannelEventResponseMsg(cer);
-					Serial.println("Received Msg: ChannelEventResponse");
-					Serial.print("Channel: ");
-					Serial.println(cer.getChannelNumber());
-					parseEventMessage(cer.getCode());
-					break;
-				}
-
-				case START_UP_MESSAGE:
-				{
-					StartUpMessage sum = StartUpMessage();
-					ant.getResponse().getStartUpMsg(sum);
-					Serial.println("Received Msg: StartupMessage");
-					Serial.print("Message: ");
-					Serial.println(sum.getMessage());
-					break;
-				}
-
-				case BROADCAST_DATA:
-				{
-					BroadcastData bd = BroadcastData();
-					ant.getResponse().getBroadcastDataMsg(bd);
-					Serial.println("Received Msg: BroadcastData");
-					Serial.print("Channel: ");
-					Serial.println(bd.getChannelNumber());
-					Serial.print("Data:[");
-					Serial.print(bd.getData(0), HEX);
-					for (uint8_t i = 1; i < 8; i++)
-					{
-						Serial.print(", ");
-						Serial.print(bd.getData(i), HEX);
-					}
-					Serial.println("]");
-					break;
-					}
-
-				default:
-					Serial.print("Undefined Message: ");
-					Serial.println(msgId, HEX);
-					break;
+		uint8_t msgId = ant.getResponse().getMsgId();
+		switch (msgId) {
+			case CHANNEL_EVENT:
+			{
+				ChannelEventResponse cer = ChannelEventResponse();
+				ant.getResponse().getChannelEventResponseMsg(cer);
+				Serial.println("Received Msg: ChannelEventResponse");
+				Serial.print("Channel: ");
+				Serial.println(cer.getChannelNumber());
+				parseEventMessage(cer.getCode());
+				break;
 			}
+
+			case START_UP_MESSAGE:
+			{
+				StartUpMessage sum = StartUpMessage();
+				ant.getResponse().getStartUpMsg(sum);
+				Serial.println("Received Msg: StartupMessage");
+				Serial.print("Message: ");
+				Serial.println(sum.getMessage());
+				break;
+			}
+
+			default:
+				Serial.print("Undefined Message: ");
+				Serial.println(msgId, HEX);
+				break;
 		}
-		else if (ant.getResponse().isError())
-		{
-			Serial.print("ANT MSG ERROR: ");
-			Serial.println(ant.getResponse().getErrorCode());
-		}
+	}
+	else if (ant.getResponse().isError())
+	{
+		Serial.print("ANT MSG ERROR: ");
+		Serial.println(ant.getResponse().getErrorCode());
 	}
 }
 
 void parseEventMessage(uint8_t code)
 {
+	BroadcastMsg bm;
 	Serial.print("Code: ");
 	switch (code)
 	{
@@ -149,16 +136,17 @@ void parseEventMessage(uint8_t code)
 			Serial.println("RESPONSE_NO_ERROR");
 			break;
 
-		case EVENT_RX_FAIL:
-			Serial.println("EVENT_RX_FAIL");
-			break;
-
-		case EVENT_RX_SEARCH_TIMEOUT:
-			Serial.println("EVENT_RX_SEARCH_TIMEOUT");
-			break;
-
 		case EVENT_CHANNEL_CLOSED:
 			Serial.println("EVENT_CHANNEL_CLOSED");
+			break;
+
+		case EVENT_TX:
+			Serial.println("EVENT_TX");
+			buffer[0]++;
+			bm = BroadcastMsg();
+			bm.setData(buffer);
+			bm.setChannel(0);
+			ant.send(bm);
 			break;
 
 		default:
