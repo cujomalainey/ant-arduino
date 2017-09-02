91774a4ba1545f118cedbaf9ab580aa6d095e5d5
Curtis Malainey <curtis@malainey.com>
2017-09-02 00:23:12 -0700

    Rename to prevent collisions
    Rename defines to prevent collisions with ant_parameters.h

--- examples/AntVersion/AntVersion.ino
+++ examples/AntVersion/AntVersion.ino
@@ -109,119 +109,119 @@ void parseMessage() {
                 Serial.println(cap.getMaxNetworks());
 
                 Serial.println("Standard Options:");
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_RECEIVE_CHANNELS  )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_RECEIVE_CHANNELS  )
                 {
                     Serial.println("No Receive Channels");
                 }
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_TRANSMIT_CHANNELS )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_TRANSMIT_CHANNELS )
                 {
                     Serial.println("No Transmit Channels");
                 }
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_RECEIVE_MESSAGES  )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_RECEIVE_MESSAGES  )
                 {
                     Serial.println("No Receive Messages");
                 }
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_TRANSMIT_MESSAGES )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_TRANSMIT_MESSAGES )
                 {
                     Serial.println("No Transmit Messages");
                 }
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_ACKD_MESSAGES     )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_ACKD_MESSAGES     )
                 {
                     Serial.println("No Ackd Messages");
                 }
-                if ( cap.getStandardOptions() & CAPABILITIES_NO_BURST_MESSAGES    )
+                if ( cap.getStandardOptions() & CAPABILITY_NO_BURST_MESSAGES    )
                 {
                     Serial.println("No Burst Messages");
                 }
 
                 Serial.println("Advanced Options:");
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_NETWORK_ENABLED              )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_NETWORK_ENABLED              )
                 {
                     Serial.println("Network Enabled");
                 }
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_SERIAL_NUMBER_ENABLED        )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_SERIAL_NUMBER_ENABLED        )
                 {
                     Serial.println("Serial Number Enabled");
                 }
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_PER_CHANNEL_TX_POWER_ENABLED )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_PER_CHANNEL_TX_POWER_ENABLED )
                 {
                     Serial.println("Per Channel Tx Power Enabled");
                 }
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_LOW_PRIORITY_SEARCH_ENABLED  )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_LOW_PRIORITY_SEARCH_ENABLED  )
                 {
                     Serial.println("Low Priority Search Enabled");
                 }
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_SCRIPT_ENABLED               )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_SCRIPT_ENABLED               )
                 {
                     Serial.println("Script Enabled");
                 }
-                if ( cap.getAdvancedOptions(0) & CAPABILITIES_SEARCH_LIST_ENABLED          )
+                if ( cap.getAdvancedOptions(0) & CAPABILITY_SEARCH_LIST_ENABLED          )
                 {
                     Serial.println("Search List Enabled");
                 }
 
                 Serial.println("Advanced Options 2:");
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_LED_ENABLED         )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_LED_ENABLED         )
                 {
                     Serial.println("LED Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_EXT_MESSAGE_ENABLED )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_EXT_MESSAGE_ENABLED )
                 {
                     Serial.println("Ext Message Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_SCAN_MODE_ENABLED   )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_SCAN_MODE_ENABLED   )
                 {
                     Serial.println("Scan Mode Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_PROX_SEARCH_ENABLED )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_PROX_SEARCH_ENABLED )
                 {
                     Serial.println("Prox Search Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_EXT_ASSIGN_ENABLED  )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_EXT_ASSIGN_ENABLED  )
                 {
                     Serial.println("Ext Assign Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_FS_ANTFS_ENABLED    )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_FS_ANTFS_ENABLED    )
                 {
                     Serial.println("FS ANTFS Enabled");
                 }
-                if ( cap.getAdvancedOptions(1) & CAPABILITIES_FIT1_ENABLED        )
+                if ( cap.getAdvancedOptions(1) & CAPABILITY_FIT1_ENABLED        )
                 {
                     Serial.println("FIT1 Enabled");
                 }
 
                 Serial.println("Advanced Options 3:");
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_ADVANCED_BURST_ENABLED         )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_ADVANCED_BURST_ENABLED         )
                 {
                     Serial.println("Advanced Burst Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_EVENT_BUFFERING_ENABLED        )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_EVENT_BUFFERING_ENABLED        )
                 {
                     Serial.println("Event Buffering Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_EVENT_FILTERING_ENABLED        )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_EVENT_FILTERING_ENABLED        )
                 {
                     Serial.println("Event Filtering Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_HIGH_DUTY_SEARCH_ENABLED       )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_HIGH_DUTY_SEARCH_ENABLED       )
                 {
                     Serial.println("High Duty Search Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_SEARCH_SHARING_ENABLED         )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_SEARCH_SHARING_ENABLED         )
                 {
                     Serial.println("Search Sharing Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_SELECTIVE_DATA_UPDATES_ENABLED )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_SELECTIVE_DATA_UPDATES_ENABLED )
                 {
                     Serial.println("Selective Data Updates Enabled");
                 }
-                if ( cap.getAdvancedOptions(2) & CAPABILITIES_ENCRYPTED_CHANNEL_ENABLED      )
+                if ( cap.getAdvancedOptions(2) & CAPABILITY_ENCRYPTED_CHANNEL_ENABLED      )
                 {
                     Serial.println("Encrypted Channel Enabled");
                 }
 
                 Serial.println("Advanced Options 4:");
-                if ( cap.getAdvancedOptions(3) & CAPABILITIES_RFACTIVE_NOTIFICATION_ENABLED )
+                if ( cap.getAdvancedOptions(3) & CAPABILITY_RFACTIVE_NOTIFICATION_ENABLED )
                 {
                     Serial.println("RfActive Notification Enabled");
                 }
