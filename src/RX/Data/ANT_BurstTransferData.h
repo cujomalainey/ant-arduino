c53820853c6ed5f2aefd9a3fe44608cfc6caac53
Curtis Malainey <curtis@malainey.com>
2017-09-02 21:57:59 -0700

    Implement BurstTransferData
    Implement the BurstTransferData message for the callback system

--- src/RX/Data/ANT_BurstTransferData.h
+++ src/RX/Data/ANT_BurstTransferData.h
@@ -1,6 +1,10 @@
 #ifndef ANT_BURSTTRANSFERDATA_h
 #define ANT_BURSTTRANSFERDATA_h
 
+#include <RX/ANT_AntRxDataResponse.h>
+
+#include <ANT_defines.h>
+
 /**
  * Represents a BurstTransferData message
  */
@@ -10,11 +14,12 @@ public:
     /**
      * Returns source channel
      */
-    uint8_t getChanneNumber();
+    uint8_t getChannelNumber();
     /**
      * Returns sepcified byte of data from payload
      */
     uint8_t getData(uint8_t index);
+    uint8_t getDataLength();
     uint8_t getExtendedDataLength();
     uint8_t getExtendedData(uint8_t index);
 
