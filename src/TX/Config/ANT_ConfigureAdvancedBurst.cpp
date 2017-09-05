d40f47f4eeff25e75255944a1bc9f4d14e68e1cc
Curtis Malainey <curtis@malainey.com>
2017-09-04 23:03:14 -0700

    Add ConfigureAdvancedBurst skeleton

--- src/TX/Config/ANT_ConfigureAdvancedBurst.cpp
+++ src/TX/Config/ANT_ConfigureAdvancedBurst.cpp
@@ -2,4 +2,80 @@
 
 #include <ANT_private_defines.h>
 
-// TODO
+ConfigureAdvancedBurst::ConfigureAdvancedBurst() {
+
+}
+
+ConfigureAdvancedBurst::ConfigureAdvancedBurst(uint8_t enable, uint8_t maxPacketLength) {
+    setEnable(enable);
+    setMaxPacketLength(maxPacketLength);
+}
+
+void ConfigureAdvancedBurst::setEnable(uint8_t enable) {
+    _enable = enable;
+}
+
+void ConfigureAdvancedBurst::setMaxPacketLength(uint8_t maxPacketLength) {
+    _maxPacketLength = maxPacketLength;
+}
+
+void ConfigureAdvancedBurst::setRequiredFeatures(uint32_t requiredFeatures) {
+    _requiredFeatures = requiredFeatures;
+}
+
+void ConfigureAdvancedBurst::setOptionalFeatures(uint32_t optionalFeatures) {
+    _optionalFeatures = optionalFeatures;
+}
+
+void ConfigureAdvancedBurst::setStallCount(uint16_t stallCount) {
+    // TODO handle extentions
+    _stallCount = stallCount;
+}
+
+void ConfigureAdvancedBurst::setRetryCount(uint8_t retryCount) {
+    // TODO handle extentions
+    _retryCount = retryCount;
+}
+
+uint8_t ConfigureAdvancedBurst::getEnable() {
+    return enable;
+}
+
+uint8_t ConfigureAdvancedBurst::getMaxPacketLength() {
+    return maxPacketLength;
+}
+
+uint32_t ConfigureAdvancedBurst::getRequiredFeatures() {
+    return _requiredFeatures;
+}
+
+uint32_t ConfigureAdvancedBurst::getOptionalFeatures() {
+    return _optionalFeatures;
+}
+
+uint16_t ConfigureAdvancedBurst::getStallCount() {
+    return _stallCount;
+}
+
+uint8_t ConfigureAdvancedBurst::getRetryCount() {
+    return _retryCount;
+}
+
+uint8_t ConfigureAdvancedBurst::getData(uint8_t pos) {
+    // TODO
+    return 0;
+}
+
+uint8_t ConfigureAdvancedBurst::getDataLength() {
+    // TODO
+    return 0;
+}
+
+#ifdef NATIVE_API_AVAILABLE
+
+uint8_t ConfigureAdvancedBurst::execute() {
+    // TODO
+    return 0;
+}
+
+#endif // NATIVE_API_AVAILABLE
--- src/TX/Config/ANT_ConfigureAdvancedBurst.h
+++ src/TX/Config/ANT_ConfigureAdvancedBurst.h
@@ -1,6 +1,41 @@
 #ifndef ANT_CONFIGUREADVANCEDBURST_h
 #define ANT_CONFIGUREADVANCEDBURST_h
 
-// TODO
+#include <TX/ANT_AntRequest.h>
+
+/**
+ * Represents a configure advanced burst packet
+ *
+ */
+class ConfigureAdvancedBurst : public AntRequest {
+public:
+    ConfigureAdvancedBurst();
+    ConfigureAdvancedBurst(uint8_t enable, uint8_t maxPacketLength);
+    void setEnable(uint8_t enable);
+    void setMaxPacketLength(uint8_t maxPacketLength);
+    void setRequiredFeatures(uint32_t requiredFeatures);
+    void setOptionalFeatures(uint32_t optionalFeatures);
+    void setStallCount(uint16_t stallCount);
+    void setRetryCount(uint8_t retryCount);
+    uint8_t getEnable();
+    uint8_t getMaxPacketLength();
+    uint32_t getRequiredFeatures();
+    uint32_t getOptionalFeatures();
+    uint16_t getStallCount();
+    uint8_t getRetryCount();
+    uint8_t getData(uint8_t pos);
+    uint8_t getDataLength();
+#ifdef NATIVE_API_AVAILABLE
+    uint8_t execute();
+#endif
+protected:
+    uint8_t _enable;
+    uint8_t _maxPacketLength;
+    uint32_t _requiredFeatures;
+    uint32_t _optionalFeatures;
+    uint16_t _stallCount;
+    uint8_t _retryCount;
+    uint8_t _extended;
+};
 
 #endif // ANT_CONFIGUREADVANCEDBURST_h
