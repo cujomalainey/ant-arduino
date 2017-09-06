7c0d71b6993c3610c55e95247660da59b7d2551e
Curtis Malainey <curtis@malainey.com>
2017-09-05 23:16:58 -0700

    Add FrequencyAgility Skeleton

--- src/TX/Config/ANT_FrequencyAgility.h
+++ src/TX/Config/ANT_FrequencyAgility.h
@@ -1,6 +1,37 @@
 #ifndef ANT_FREQUENCYAGILITY_h
 #define ANT_FREQUENCYAGILITY_h
 
-// TODO
+#include <TX/ANT_AntRequest.h>
+
+/**
+ * Represents a configure event filter packet
+ *
+ */
+class FrequencyAgility : public AntRequest {
+public:
+    FrequencyAgility();
+    FrequencyAgility(uint8_t channel);
+    FrequencyAgility(uint8_t channel, uint8_t freq1, uint8_t freq2, uint8_t freq3);
+    void setChannel(uint8_t channel);
+    void setFrequency(uint8_t index, uint8_t freq);
+    void setFrequency1(uint8_t freq);
+    void setFrequency2(uint8_t freq);
+    void setFrequency3(uint8_t freq);
+    uint8_t getChannel();
+    uint8_t getFrequency(uint8_t index);
+    uint8_t getFrequency1();
+    uint8_t getFrequency2();
+    uint8_t getFrequency3();
+    uint8_t getData(uint8_t pos);
+    uint8_t getDataLength();
+#ifdef NATIVE_API_AVAILABLE
+    uint8_t execute();
+#endif // NATIVE_API_AVAILABLE
+protected:
+    uint8_t _channel;
+    uint8_t _freq1;
+    uint8_t _freq2;
+    uint8_t _freq3;
+};
 
 #endif // ANT_FREQUENCYAGILITY_h
