2f3aa847d13a3b4f49ccee93dfa9e7459ca08b79
Curtis Malainey <curtis@malainey.com>
2017-09-06 21:14:46 -0700

    Add ProximitySearch skeleton

--- src/TX/Config/ANT_ProximitySearch.cpp
+++ src/TX/Config/ANT_ProximitySearch.cpp
@@ -2,4 +2,50 @@
 
 #include <ANT_private_defines.h>
 
-// TODO
+ProximitySearch::ProximitySearch() : AntRequest(PROXIMITY_SEARCH) {
+
+}
+
+ProximitySearch::ProximitySearch(uint8_t channel) : AntRequest(PROXIMITY_SEARCH) {
+    setChannel(channel);
+}
+
+ProximitySearch::ProximitySearch(uint8_t channel, uint8_t searchThreshold) : AntRequest(PROXIMITY_SEARCH) {
+    setChannel(channel);
+    setSearchThreshold(searchThreshold);
+}
+
+void ProximitySearch::setChannel(uint8_t channel) {
+    _channel = channel;
+}
+
+void ProximitySearch::setSearchThreshold(uint8_t searchThreshold) {
+    _searchThreshold = searchThreshold;
+}
+
+uint8_t ProximitySearch::getChannel() {
+    return _channel;
+}
+
+uint8_t ProximitySearch::getSearchThreshold() {
+    return _searchThreshold;
+}
+
+uint8_t ProximitySearch::getData(uint8_t pos) {
+    // TODO
+    return 0;
+}
+
+uint8_t ProximitySearch::getDataLength() {
+    // TODO
+    return 0;
+}
+
+#ifdef NATIVE_API_AVAILABLE
+
+uint8_t ProximitySearch::execute() {
+    // TODO
+    return 0;
+}
+
+#endif // NATIVE_API_AVAILABLE
--- src/TX/Config/ANT_ProximitySearch.h
+++ src/TX/Config/ANT_ProximitySearch.h
@@ -1,6 +1,27 @@
 #ifndef ANT_PROXIMITYSEARCH_h
 #define ANT_PROXIMITYSEARCH_h
 
-// TODO
+/**
+ * Represents a set proximity search packet
+ *
+ */
+class ProximitySearch : public AntRequest {
+public:
+    ProximitySearch();
+    ProximitySearch(uint8_t channel);
+    ProximitySearch(uint8_t channel, uint8_t searchThreshold);
+    void setChannel(uint8_t channel);
+    void setSearchThreshold(uint8_t searchThreshold);
+    uint8_t getChannel();
+    uint8_t getSearchThreshold();
+    uint8_t getData(uint8_t pos);
+    uint8_t getDataLength();
+#ifdef NATIVE_API_AVAILABLE
+    uint8_t execute();
+#endif // NATIVE_API_AVAILABLE
+protected:
+    uint8_t _channel;
+    uint8_t _searchThreshold
+};
 
 #endif // ANT_PROXIMITYSEARCH_h
