4ea8c8f1e518cf1eb3e5499de27574770577636e
Curtis Malainey <curtis@malainey.com>
2017-09-06 22:01:16 -0700

    Add all messages headers to header path

--- src/TX/ANT_messages.h
+++ src/TX/ANT_messages.h
@@ -1,15 +1,49 @@
 /* Available TX Messages */
 // Config (TX)
+#include <TX/Config/ANT_AddChannelIdToList.h>
+#include <TX/Config/ANT_AddEncryptionIdToList.h>
 #include <TX/Config/ANT_AssignChannel.h>
 #include <TX/Config/ANT_ChannelId.h>
 #include <TX/Config/ANT_ChannelPeriod.h>
 #include <TX/Config/ANT_ChannelRfFrequency.h>
+#include <TX/Config/ANT_ChannelSearchPriority.h>
+#include <TX/Config/ANT_ChannelSearchSharing.h>
+#include <TX/Config/ANT_ConfigEncryptionIdList.h>
+#include <TX/Config/ANT_ConfigList.h>
+#include <TX/Config/ANT_ConfigureAdvancedBurst.h>
+#include <TX/Config/ANT_ConfigureFilter.h>
+#include <TX/Config/ANT_ConfigureSelectiveDataUpdates.h>
+#include <TX/Config/ANT_EnableExtRxMessages.h>
+#include <TX/Config/ANT_EnableSingleChannelEncryption.h>
+#include <TX/Config/ANT_EventFilter.h>
+#include <TX/Config/ANT_FrequencyAgility.h>
+#include <TX/Config/ANT_LibConfig.h>
+#include <TX/Config/ANT_LowPrioritySearchTimeout.h>
+#include <TX/Config/ANT_ProximitySearch.h>
+#include <TX/Config/ANT_SearchTimeout.h>
+#include <TX/Config/ANT_SearchWaveform.h>
+#include <TX/Config/ANT_SerialNumberSetChannelId.h>
+#include <TX/Config/ANT_SetChannelTransmitPower.h>
+#include <TX/Config/ANT_SetEncryptionInfo.h>
+#include <TX/Config/ANT_SetEncryptionKey.h>
 #include <TX/Config/ANT_SetNetworkKey.h>
+#include <TX/Config/ANT_SetSelectiveDataUpdateMask.h>
+#include <TX/Config/ANT_TransmitPower.h>
+#include <TX/Config/ANT_UnAssignChannel.h>
 
 // Control (TX)
+#include <TX/Control/ANT_CloseChannel.h>
 #include <TX/Control/ANT_OpenChannel.h>
+#include <TX/Control/ANT_OpenRxScanMode.h>
 #include <TX/Control/ANT_RequestMessage.h>
 #include <TX/Control/ANT_ResetSystem.h>
 
 // Data (TX)
-#include <TX/Data/ANT_BroadcastMsg.h>
+#include <TX/Data/ANT_AcknowledgedDataMsg.h>
+#include <TX/Data/ANT_AdvancedBurstDataMsg.h>
+#include <TX/Data/ANT_BroadcastDataMsg.h>
+#include <TX/Data/ANT_BurstTransferDataMsg.h>
+
+// Test (TX)
+#include <TX/Data/ANT_CWInit.h>
+#include <TX/Data/ANT_CWTest.h>
