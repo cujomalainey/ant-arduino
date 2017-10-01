88e97d9054a9af345faf964ae53f5c2af841d6fd
Curtis Malainey <curtis@malainey.com>
2017-09-30 17:12:10 -0700

    Implement encryption mode parameters

--- src/RX/Status/ANT_EncryptionModeParameters.h
+++ src/RX/Status/ANT_EncryptionModeParameters.h
@@ -11,7 +11,21 @@
 class EncryptionModeParameters : public AntResponse {
 public:
     EncryptionModeParameters();
-    // TODO
+
+    uint8_t getRequestedModeParameter();
+    /**
+     * Only call if getRequestedModeParameter == 0
+     */
+    uint8_t getMaxSupportedEncryptionMode();
+    /**
+     * Only call if getRequestedModeParameter == 1
+     */
+    uint32_t getUniqueIdentifier();
+    /**
+     * Only call if getRequestedModeParameter == 2
+     */
+    char* getUserInformationString();
+
     static const uint8_t MSG_ID = ENCRYPTION_MODE_PARAMETERS;
 };
 
