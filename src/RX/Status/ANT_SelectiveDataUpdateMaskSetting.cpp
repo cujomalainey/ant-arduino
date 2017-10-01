6b345147c90b9f8fa25144a1d0d53a47d5ea7131
Curtis Malainey <curtis@malainey.com>
2017-09-30 17:22:24 -0700

    Impletment SDUMask

--- src/RX/Status/ANT_SelectiveDataUpdateMaskSetting.cpp
+++ src/RX/Status/ANT_SelectiveDataUpdateMaskSetting.cpp
@@ -3,3 +3,11 @@
 SelectiveDataUpdateMaskSetting::SelectiveDataUpdateMaskSetting() : AntResponse() {
 
 }
+
+uint8_t SelectiveDataUpdateMaskSetting::getSDUMaskNumber() {
+    return getFrameData()[0];
+}
+
+uint8_t* SelectiveDataUpdateMaskSetting::getSDUMask() {
+    return getFrameData() + 1;
+}
