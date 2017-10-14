7c41533062696bb0d03dcfe76dd9e051522ff5ad
Curtis Malainey <curtis@malainey.com>
2017-10-14 11:25:21 -0700

    Complete AddChannelIdToList Tests

--- test/TX_AddChannelIdToList/Test_AddChannelIdToList.cpp
+++ test/TX_AddChannelIdToList/Test_AddChannelIdToList.cpp
@@ -1,36 +1,63 @@
 #include "unity.h"
+#include "ANT.h"
+#include "Util/Testing.h"
 
 #ifdef UNIT_TEST
 
-void setUp(void) {
-// set stuff up here
+const uint8_t expectedOut[] = {0xA4, 6, 0x59, 2, 0x44, 0x33, 120, 34, 2, 214};
+Stream mock_stream = Stream(NULL, expectedOut);
+AntWithCallbacks ant = AntWithCallbacks();
+AddChannelIdToList msg;
+
+void test_constructors(void) {
+    msg = AddChannelIdToList(2, 3344, 120, 34, 2);
+    TEST_ASSERT_EQUAL_UINT8(2, msg.getChannel());
+    TEST_ASSERT_EQUAL_UINT16(3344, msg.getDeviceNumber());
+    TEST_ASSERT_EQUAL_UINT8(120, msg.getDeviceType());
+    TEST_ASSERT_EQUAL_UINT8(34, msg.getTransmissionType());
+    TEST_ASSERT_EQUAL_UINT8(2, msg.getListIndex());
 }
 
-void tearDown(void) {
-// clean stuff up here
+void test_setChannel(void) {
+    msg.setChannel(3);
+    TEST_ASSERT_EQUAL_UINT8(3, msg.getChannel());
 }
 
-void test_function_calculator_addition2(void) {
-    TEST_ASSERT_EQUAL(32, 25 + 7);
+void test_setDeviceNumber(void) {
+    msg.setDeviceNumber(0x4455);
+    TEST_ASSERT_EQUAL_UINT16(0x4455, msg.getDeviceNumber());
 }
 
-void test_function_calculator_subtraction2(void) {
-    TEST_ASSERT_EQUAL(20, 23 - 3);
+void test_setDeviceType(void) {
+    msg.setDeviceType(100);
+    TEST_ASSERT_EQUAL_UINT8(100, msg.getDeviceType());
 }
 
-void test_function_calculator_multiplication2(void) {
-    TEST_ASSERT_EQUAL(50, 25 * 2);
+void test_setTransmisionType(void) {
+    msg.setTransmissionType(12);
+    TEST_ASSERT_EQUAL_UINT8(12, msg.getTransmissionType());
 }
 
-void test_function_calculator_division2(void) {
-    TEST_ASSERT_EQUAL(32, 100 / 3);
+void test_getDataLength(void) {
+    TEST_ASSERT_EQUAL_UINT8(6, msg.getDataLength());
+}
+
+void test_serialize(void) {
+    // Asserts are in mock stream
+    ant.begin(mock_stream);
+    msg = AddChannelIdToList(2, 0x3344, 120, 34, 2);
+    ant.send(msg);
 }
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_function_calculator_addition2);
-    RUN_TEST(test_function_calculator_subtraction2);
-    RUN_TEST(test_function_calculator_multiplication2);
+    RUN_TEST(test_constructors);
+    RUN_TEST(test_setChannel);
+    RUN_TEST(test_setDeviceNumber);
+    RUN_TEST(test_setDeviceType);
+    RUN_TEST(test_setTransmisionType);
+    RUN_TEST(test_getDataLength);
+    RUN_TEST(test_serialize);
     UNITY_END();
 
     return 0;
