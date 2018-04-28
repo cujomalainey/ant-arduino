73353e7f181adea013f7d653e28c706e3258c2e7
Curtis Malainey <curtis@malainey.com>
2018-04-28 17:04:21 -0600

    Add set encryption info

--- src/TX/Config/ANT_SetEncryptionInfo.h
+++ src/TX/Config/ANT_SetEncryptionInfo.h
@@ -11,14 +11,23 @@
 class SetEncryptionInfo : public AntRequest {
 public:
     SetEncryptionInfo();
-    SetEncryptionInfo(uint8_t channel, uint16_t waveform);
-    // TODO implement params
+    SetEncryptionInfo(uint8_t param, uint8_t* data);
+    void setParameter(uint8_t param);
+    void setDataString(uint8_t* data);
+    void setDataStringByte(uint8_t byte, uint8_t pos);
+    uint8_t getParameter();
+    void getDataString(uint8_t* arr);
+    uint8_t getDataStringByte(uint8_t pos);
     uint8_t getData(uint8_t pos);
     uint8_t getDataLength();
 #ifdef NATIVE_API_AVAILABLE
     uint8_t execute();
 #endif // NATIVE_API_AVAILABLE
 private:
+    uint8_t getDataStringSize();
+    uint8_t _setParameter;
+    uint8_t _dataString[SET_ENCRYPTION_INFO_DATA_STRING_USER_INFORMATION_STRING_SIZE];
+    // USER_INFORMATION_STRING is the largest among the possible configurations, can be used for other modes
 };
 
 #endif // ANT_SETENCRYPTIONINFO_h
